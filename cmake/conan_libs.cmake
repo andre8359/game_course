bf73f5448229660c57c8c57c7392e540d508421d
Andre Dantas <andre8359@gmail.com>
2022-02-27 00:55:45 -0300

    [01] Adds conan files and base setup for sfml

--- cmake/conan_libs.cmake
+++ cmake/conan_libs.cmake
@@ -5,18 +5,20 @@ list(APPEND CMAKE_PREFIX_PATH ${CMAKE_BINARY_DIR})
 
 if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
   message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
-  file(DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/v0.17.0/conan.cmake"
+  file(DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/release/0.17/conan.cmake"
                 "${CMAKE_BINARY_DIR}/conan.cmake")
 endif()
 
 include(${CMAKE_BINARY_DIR}/conan.cmake)
 
-conan_cmake_configure(REQUIRES fmt/6.1.2
-                      GENERATORS cmake_find_package)
+conan_cmake_run(REQUIRES sfml/2.5.1
+                BASIC_SETUP CMAKE_TARGETS
+                BUILD missing)
+
 
 conan_cmake_autodetect(settings)
 
 conan_cmake_install(PATH_OR_REFERENCE .
-                    BUILD missing
-                    REMOTE conancenter
-                    SETTINGS ${settings})
+                     BUILD missing
+                     REMOTE conancenter
+                     SETTINGS ${settings})
